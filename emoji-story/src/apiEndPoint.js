93b7be3dfbd0be104b1da1d879aa0d3d4f82a554
coreyo-git <corey.oconnor@live.com.au>
2023-09-09 10:10:18 +1000

    Updated apiEndPoint.js to also store current port

--- emoji-story/src/apiEndPoint.js
+++ emoji-story/src/apiEndPoint.js
@@ -1,3 +1,8 @@
-export const API_ENDPOINT = window && window.location && window.location.hostname + "/api";
-
+//export const API_ENDPOINT = window && window.location && window.location.hostname + "/api";
 //export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || "localhost:3000";
+export const API_ENDPOINT =
+  window &&
+  window.location &&
+  window.location.hostname +
+    (window.location.port ? ":" + window.location.port : "") +
+    "/api"; // set to current window url + port
