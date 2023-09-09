942faab2403a26bd4d88e345909081fd953cef02
coreyo-git <corey.oconnor@live.com.au>
2023-09-09 10:27:06 +1000

    Increment now create counter db if not exists

--- myapp/services/incrementAndGetVisitorCounter.js
+++ myapp/services/incrementAndGetVisitorCounter.js
@@ -1,7 +1,7 @@
 const {
     DynamoDBClient,
     ListTablesCommand,
-	CreateTableCommand,
+    CreateTableCommand,
 } = require("@aws-sdk/client-dynamodb");
 const {
     DynamoDBDocumentClient,
@@ -9,10 +9,45 @@ const {
 } = require("@aws-sdk/lib-dynamodb");
 
 // setup dynamo db configurations
-const client = new DynamoDBClient({ region: "ap-southeast-2"});
+const client = new DynamoDBClient({ region: "ap-southeast-2" });
 const dynamodb = DynamoDBDocumentClient.from(client);
-const tableName = "VisitorCounter";
+const tableName = "AT1-VisitCount-n11270209";
 const counterKey = "emoji-story";
+
+// Flag to prevent multiple table creation attempts
+let isCreatingTable = false; 
+
+// Async function to create the table if it doesn't exist
+async function createTableIfNotExists() {
+	if (isCreatingTable) {
+        // Another instance is already creating the table, so wait
+        await new Promise((resolve) => setTimeout(resolve, 2000)); // Wait for 2 second
+        return;
+    }
+
+	isCreatingTable = true;
+    const listTablesCommand = new ListTablesCommand({});
+    const existingTables = await client.send(listTablesCommand);
+
+    if (!existingTables.TableNames.includes(tableName)) {
+        const createTableParams = {
+            TableName: tableName,
+            KeySchema: [
+                { AttributeName: "Counter", KeyType: "HASH" }, // Partition key
+            ],
+            AttributeDefinitions: [
+                { AttributeName: "Counter", AttributeType: "S" }, // String attribute for the partition key
+            ],
+            ProvisionedThroughput: {
+                ReadCapacityUnits: 5, 
+                WriteCapacityUnits: 5,
+            },
+        };
+
+        const createTableCommand = new CreateTableCommand(createTableParams);
+        await client.send(createTableCommand);
+    }
+}
 
 // Async function to increment and get the visitor counter
 async function incrementAndGetVisitorCounter() {
@@ -36,6 +71,20 @@ async function incrementAndGetVisitorCounter() {
         return incrementResponse.Attributes.VisitorCount;
     } catch (error) {
         console.log("Error updating visitor count: ", error);
+
+        // Check if the error is due to the table not existing
+        if (error.name === "ResourceNotFoundException") {
+            console.log("Table does not exist, creating...");
+			
+			// Create the table
+			await createTableIfNotExists();
+            
+			// Retry the increment operation
+            return await incrementAndGetVisitorCounter();           
+        }
+
+        // Handle other errors as needed
+        throw error; // Re-throw the error for higher-level handling
     }
 }
 
